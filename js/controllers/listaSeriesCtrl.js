e697aedbaccd50fc5003eaea394f3b10d032481c
petcomputacao <matheus.sousa@ccc.ufcg.edu.br>
2017-06-20 17:03:10 -0300

    sei la

--- js/controllers/listaSeriesCtrl.js
+++ js/controllers/listaSeriesCtrl.js
@@ -2,20 +2,20 @@ angular.module("listaSeries").controller("listaSeriesCtrl", function ($scope,$ht
 	$scope.app = "Lista De Séries";
 	$scope.series = [];
 	$scope.minhasSeries = [];
-
+	$scope.watchlist = [];
 
 	$scope.pesquisarSerie = function(serie){
 		$http.get("https://omdbapi.com/?s=" + serie + "&apikey=93330d3c&type=series").then(function(response) {
 			if(response.data.Response == "True"){
 				$scope.series = response.data.Search;
 
-
+			delete $scope.serie;	
 			}else{
 				alert("a série nao existe");
 			};
 
 
-			delete $scope.serie;
+			
 		}, function() {
 
 		});
@@ -26,27 +26,41 @@ angular.module("listaSeries").controller("listaSeriesCtrl", function ($scope,$ht
 
 
 
-	$scope.verificaArray = function(serie){
-		for (var i = $scope.minhasSeries.length - 1; i >= 0; i--) {
-			if($scope.minhasSeries[i]== serie){
+	$scope.verificaArray = function(serie,array){
+		for (var i = array.length - 1; i >= 0; i--) {
+			if(array[i].Title == serie.Title){
 				return true;
 				
 			}
 		}
 		return false;
 	}
+	$scope.addSerieWatch = function(serie){
+		if($scope.verificaArray(serie,$scope.watchlist ) & $scope.verificaArray(serie,$scope.minhasSeries)){
 
-	$scope.addSerie = function(serie){
-		if($scope.verificaArray(serie)){
+			alert("série já pertence ao seu watchlist");
+		}else{
+			$scope.watchlist.push(serie);
+		}
+	}
+
+	$scope.addSeriePerfil = function(serie){
+		if($scope.verificaArray(serie,$scope.minhasSeries)){
 
 			alert("série já pertence ao seu perfil");
 		}else{
 			$scope.minhasSeries.push(serie);
 		}
+		$scope.removeSerie(serie,$scope.watchlist);
 	}
 
-	$scope.removeSerie = function(serie){
-		
+	$scope.removeSerie = function(serie,array){
+		for (var i = array.length - 1; i >= 0; i--) {
+			if(array[i].Title == serie.Title){
+				array.splice(i, 1);
+				
+			}
+		}
 	}	
 
 
